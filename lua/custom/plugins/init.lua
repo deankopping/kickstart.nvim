ab82528039410b7547e4af0b23606d16c8c1386f
Dean Kopping <dean@teamgeek.io>
2025-10-09 13:51:31 +0200

    feat: prettier for typescript

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,6 +6,7 @@ return { -- ~/.config/nvim/lua/custom/plugins/init.lua
   -- other plugins...
   { 'tpope/vim-fugitive' },
   { 'rebelot/kanagawa.nvim' },
+  --  { 'tpope/vim-vinegar' },
 
   require 'custom.plugins.oil',
   require 'custom.plugins.alpha',
