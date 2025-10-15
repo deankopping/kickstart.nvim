2fae0e1b3625d541c14185facd017f4a16b87e40
Dean Kopping <dean@teamgeek.io>
2025-10-15 13:38:10 +0200

    fix: autotag config

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -7,12 +7,13 @@ return {
   {
     'windwp/nvim-ts-autotag',
     event = 'InsertEnter',
-    config = function()
-      require('nvim-ts-autotag').setup {
+    opts = {
+      autotag = {
+        enable = true,
         enable_close = true,
         enable_rename = true,
         enable_close_on_slash = false,
-      }
-    end,
+      },
+    },
   },
 }
