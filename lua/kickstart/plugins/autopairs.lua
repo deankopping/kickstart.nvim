e80f1404093a0fec053dee2952329e16f61ad33b
Dean Kopping <dean@teamgeek.io>
2025-10-15 10:42:26 +0200

    fix: updated autotag config

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -1,6 +1,3 @@
--- autopairs with autotag
--- https://github.com/windwp/nvim-autopairs
--- https://github.com/windwp/nvim-ts-autotag
 return {
   {
     'windwp/nvim-autopairs',
@@ -10,18 +7,12 @@ return {
   {
     'windwp/nvim-ts-autotag',
     event = 'InsertEnter',
-    opts = {
-      -- Defaults
-      enable_close = true, -- Auto close tags
-      enable_rename = true, -- Auto rename pairs of tags
-      enable_close_on_slash = false, -- Auto close on trailing </
-    },
-    -- If you want to specify filetypes manually:
-    -- opts = {
-    --   filetypes = {
-    --     'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact',
-    --     'svelte', 'vue', 'tsx', 'jsx', 'xml', 'markdown',
-    --   },
-    -- },
+    config = function()
+      require('nvim-ts-autotag').setup {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      }
+    end,
   },
 }
